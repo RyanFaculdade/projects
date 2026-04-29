66de198756a97c1540d038cb197b8122c560ac06
RyanFaculdade <ryan.hangai@aluno.impacta.edu.br>
2026-04-29 10:18:45 -0300

    Primeira metadade da aula

---
+++ algoritimo_aula_01/src/main.py
@@ -0,0 +1,19 @@
+from strings_utils import *
+
+
+def main():
+    print('Main')
+
+    string = input('Digite uma string: ')
+
+    print(analisar_string(string))
+    
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ algoritimo_aula_01/src/strings_utils.py
@@ -0,0 +1,14 @@
+def analisar_string(texto: str): 
+    return { 
+       'tamanho': len(texto), 
+        'primeiro': texto[0], 
+        'ultimo': texto[-1], 
+        'maiusculo': texto.upper(), 
+        'minusculo': texto.lower()
+ }
+
+def main():
+    analisar_string()
+
+if __name__ == '__main__':
+    main()
