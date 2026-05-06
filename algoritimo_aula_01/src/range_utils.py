06c9173ef15d0bc7d5c358613e7db2cb1c05a637
RyanFaculdade <ryan.hangai@aluno.impacta.edu.br>
2026-05-06 11:39:19 -0300

    Delete algoritimo_aula_01/src directory
    Renomeie o arquivo para algoritimo_01

--- algoritimo_aula_01/src/range_utils.py
+++
@@ -1,9 +0,0 @@
-def gerar_sequencia(inicio, fim, passo): 
- return list(range(inicio, fim, passo))
-
-
-
-
-
-if __name__ == '__main__':
-    print('É uma lib')
--- algoritimo_aula_01/src/tuplas_utils.py
+++
@@ -1,8 +0,0 @@
-def criar_tupla(): 
-    return (10, "Python", True)
-
-
-
-
-if __name__ == '__main__':
-    print('É uma lib')
